f34ef34011cdaef3e5bbfe3bc556f357a7c8ae85
Siissioe123 <140965283+siissioe123@users.noreply.github.com>
2024-08-20 13:59:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,10 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+If you got any question/concern, conact me at siissioe123@gmail.com
+"""
 
 import subprocess
 import time
